d23ee288340ae8a2b05e9115d1167412c2d8f496
Sanganu <kppkmsanu@gmail.com>
2020-06-20 13:04:22 -0600

    Employeejs file

--- src/pages/Employee.js
+++ src/pages/Employee.js
@@ -1,20 +1,39 @@
 import React, { Component } from "react";
 import axios from"axios";
+import EmployeeData from "./EmployeeData";
 
 class Employee extends Component{
     state = {
-
+        rows: []
     }
-    compoenetDidMount = () => {
-        axios.get("")
+    componentDidMount = () => {
+        axios.get("https://randomuser.me/api/?results=200&nat=us")
         .then((employeeRecords) => {
-            console.log(employeeRecords.results);
+            console.log(employeeRecords);
+            let emprec = employeeRecords.data.results;
+            let employeedata= []
+            for(let i=0;i<emprec.length;i++){
+                let record = {
+                    fname: emprec[i].name.first,
+                    lname: emprec[i].name.last,
+                    email: emprec[i].email,
+                    phone: emprec[i].nat,
+                    profile:`<img src = ${emprec[i].picture.medium} />`
+                }
+                employeedata.push(record)
+            }
+            this.setState({rows: employeedata},() => {
+                console.log("State",this.state.rows)
+            })
         })
     }
     render(){
-        reutn(<div className="jumbotron bg-warning">
+        return (<div className="jumbotron bg-warning">
         <h5> Employee Directory</h5>
         <h6>An app using axios, mdbreact Datatable</h6>
+        <EmployeeData rows={this.state.rows}/>
             </div>)
     }
 }
+
+export default Employee;
---
+++ src/pages/EmployeeData.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { MDBDataTableV5 } from 'mdbreact';
+
+function EmployeeData(props){
+   let datatable = {
+        columns: [
+            {
+                label: "First name",
+                field:"fname",
+                width:150,
+                attributes:{
+                    "aria-controls":"DataTable",
+                    "aria-label":"First Name"
+                }
+            },
+            {
+                label:"Last name",
+                field:"lname",
+                width:150
+            },
+            {
+                label:"Email",
+                field:"email"
+            },
+            {
+                label:"Phone",
+                field:"phone"
+            },
+            {
+                label:"Profile",
+                field:"profile"
+            }
+        ],
+        rows: props.rows
+    }
+ 
+        return(<div>
+            <MDBDataTableV5 hover data={datatable}/>
+        </div>)
+ 
+}
+
+
+export default EmployeeData;
