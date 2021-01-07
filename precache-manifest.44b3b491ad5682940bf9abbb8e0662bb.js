c7af66e16e9c7bac425eac21b25ffcec7980a48e
Sanganu <kppkmsanu@gmail.com>
2021-01-06 22:33:44 -0700

    Updates

---
+++ precache-manifest.44b3b491ad5682940bf9abbb8e0662bb.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6524d2113794c6fc4e5e97b554b3bb01",
+    "url": "/reactempaxios/index.html"
+  },
+  {
+    "revision": "2405b017a6cbf806b2b7",
+    "url": "/reactempaxios/static/css/2.c61b1e8f.chunk.css"
+  },
+  {
+    "revision": "920b9d16438d021a1883",
+    "url": "/reactempaxios/static/css/main.ba47ad55.chunk.css"
+  },
+  {
+    "revision": "2405b017a6cbf806b2b7",
+    "url": "/reactempaxios/static/js/2.74f686f7.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/reactempaxios/static/js/2.74f686f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "920b9d16438d021a1883",
+    "url": "/reactempaxios/static/js/main.6df0a8f0.chunk.js"
+  },
+  {
+    "revision": "74f8135cdbc0937dc617",
+    "url": "/reactempaxios/static/js/runtime-main.e3ec77b8.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/reactempaxios/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/reactempaxios/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/reactempaxios/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/reactempaxios/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/reactempaxios/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/reactempaxios/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/reactempaxios/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/reactempaxios/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/reactempaxios/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/reactempaxios/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/reactempaxios/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/reactempaxios/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/reactempaxios/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/reactempaxios/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/reactempaxios/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/reactempaxios/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/reactempaxios/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
--- src/components/Home.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Home(){
-    return(<div className="container text-center mx-auto landing">
-    <h3>Welcome to Employee Directory</h3>
-    </div>)
-}
-export default Home;
